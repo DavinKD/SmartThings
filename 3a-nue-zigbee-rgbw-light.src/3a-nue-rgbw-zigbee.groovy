df34b43c646a4273e7ca927f99c697a20b968623
DavinKD <37185595+DavinKD@users.noreply.github.com>
2019-02-21 10:00:14 -0700

    Update 3a-nue-rgbw-zigbee.groovy

--- 3a-nue-zigbee-rgbw-light.src/3a-nue-rgbw-zigbee.groovy
+++ 3a-nue-zigbee-rgbw-light.src/3a-nue-rgbw-zigbee.groovy
@@ -9,6 +9,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 for the specific language governing permissions and limitations under the License.
 Updated by Kevin X- 3A Smart Home on 1st Jun 2018
+Updated by Davin Dameron on 20190221
 */
 import groovy.transform.Field
 
