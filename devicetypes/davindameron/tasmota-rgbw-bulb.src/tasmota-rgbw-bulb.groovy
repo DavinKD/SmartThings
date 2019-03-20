f3e046a1ad4b2c1da659112be7434780d3c44e1e
DavinKD <37185595+DavinKD@users.noreply.github.com>
2019-03-20 12:03:48 -0700

    Update tasmota-rgbw-bulb.groovy

--- devicetypes/davindameron/tasmota-rgbw-bulb.src/tasmota-rgbw-bulb.groovy
+++ devicetypes/davindameron/tasmota-rgbw-bulb.src/tasmota-rgbw-bulb.groovy
@@ -151,8 +151,8 @@ def execute(String command){
 				}
 				if (json."HSBColor") {
 					def values = json."HSBColor".split(',')
-					Integer iHue = values[0].toInteger
-					Integer iSaturation = values[1].toInteger
+					Integer iHue = values[0].toInteger()
+					Integer iSaturation = values[1].toInteger()
 					iHue = iHue / 360 * 100
 					doLogging "SendEvent hue to ${iHue}"
 					doLogging "SendEvent saturation to ${iSaturation}"
