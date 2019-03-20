aa1f82c04d722fe8066132359757669ff765b814
DavinKD <37185595+DavinKD@users.noreply.github.com>
2019-03-20 11:13:19 -0700

    Fixed HSBColor

--- devicetypes/davindameron/tasmota-rgbw-bulb.src/tasmota-rgbw-bulb.groovy
+++ devicetypes/davindameron/tasmota-rgbw-bulb.src/tasmota-rgbw-bulb.groovy
@@ -150,7 +150,7 @@ def execute(String command){
 					
 				}
 				if (json."HSBColor") {
-					def hsbColor[] = json."HSBColor".split(',')
+					Strting hsbColor[] = json."HSBColor".split(',')
 					def hue = hsbColor[0] / 360 * 100
 					def saturation = hsbColor[1]
 					doLogging "SendEvent hue to ${hue}"
