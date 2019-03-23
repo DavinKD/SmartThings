920f3b1a32ebffd70a0e255754e77cda8f51b5fd
DavinKD <37185595+DavinKD@users.noreply.github.com>
2019-03-22 19:52:45 -0700

    Update tasmota-energy-monitoring-plug.groovy

--- devicetypes/davindameron/tasmota-energy-monitoring-plug.src/tasmota-energy-monitoring-plug.groovy
+++ devicetypes/davindameron/tasmota-energy-monitoring-plug.src/tasmota-energy-monitoring-plug.groovy
@@ -263,11 +263,6 @@ def poll() {
 
 def refresh() {
 	doLogging "refresh()"
-	//sendCommand("Status", "0", refreshCallback)
-}
-
-def refresh2() {
-	doLogging "refresh()"
 	sendCommand("Status", "0", refreshCallback)
 }
 
