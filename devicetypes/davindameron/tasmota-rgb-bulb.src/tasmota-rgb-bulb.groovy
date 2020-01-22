cd4660cfe098542f3a20fa6c22a00334e193e639
DavinKD <37185595+DavinKD@users.noreply.github.com>
2020-01-21 20:47:05 -0700

    Changed definition

--- devicetypes/davindameron/tasmota-rgb-bulb.src/tasmota-rgb-bulb.groovy
+++ devicetypes/davindameron/tasmota-rgb-bulb.src/tasmota-rgb-bulb.groovy
@@ -25,7 +25,7 @@ import groovy.transform.Field
 metadata {
 	//Based on work by Brett Sheleski for Tasomota-Power
 
-	definition(name: "Tasmota RGB Bulb", namespace: "davindameron", author: "Davin Dameron", ocfDeviceType: "oic.d.smartplug") {
+	definition(name: "Tasmota RGB Bulb", namespace: "davindameron", author: "Davin Dameron", vid:"generic-rgbw-color-bulb") {
 		capability "Polling"
 		capability "Refresh"
 		capability "Switch"
