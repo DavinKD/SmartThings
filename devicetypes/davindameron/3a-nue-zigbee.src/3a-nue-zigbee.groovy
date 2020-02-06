9f4e00d2f195f360f788bb022dcb33a8a00852c7
DavinKD <37185595+DavinKD@users.noreply.github.com>
2020-02-06 13:39:55 -0700

    Playing with neighborDNI

--- devicetypes/davindameron/3a-nue-zigbee.src/3a-nue-zigbee.groovy
+++ devicetypes/davindameron/3a-nue-zigbee.src/3a-nue-zigbee.groovy
@@ -169,7 +169,7 @@ def doLogging(value){
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-	doLogging "description is $description"
+	doLogging "description is $description [$device.neighborDni]"
 
     def cmds = []
 	def finalResult = zigbee.getEvent(description)
