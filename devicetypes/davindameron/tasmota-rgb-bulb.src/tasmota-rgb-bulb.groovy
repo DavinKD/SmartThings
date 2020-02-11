7f48155f13e48b88b81240a1b6cdfe9eb6c242ed
DavinKD <37185595+DavinKD@users.noreply.github.com>
2020-02-11 12:58:41 -0700

    Added rssi

--- devicetypes/davindameron/tasmota-rgb-bulb.src/tasmota-rgb-bulb.groovy
+++ devicetypes/davindameron/tasmota-rgb-bulb.src/tasmota-rgb-bulb.groovy
@@ -141,6 +141,9 @@ def execute(String command){
 					def ss = json."Wifi"."RSSI";
 					//ss = (ss*255)/100;
 					sendEvent(name: "lqi", value: ss);
+
+					def rssi = json."Wifi"."Signal";
+					sendEvent(name: "rssi", value: rssi);
 				}						
 				//Color Temp
 				if (json."CT"!=null) {
