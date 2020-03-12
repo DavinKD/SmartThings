862f447cd55ec93730e965510e0440050bcb82ab
DavinKD <37185595+DavinKD@users.noreply.github.com>
2020-03-11 20:40:03 -0700

    Update tasmota-rgbcw-bulb.groovy

--- devicetypes/davindameron/tasmota-rgbcw-bulb.src/tasmota-rgbcw-bulb.groovy
+++ devicetypes/davindameron/tasmota-rgbcw-bulb.src/tasmota-rgbcw-bulb.groovy
@@ -588,7 +588,7 @@ def setColor(Map colorHSMap) {
     doLogging "bounded hue and saturation: $boundedHue, $boundedSaturation; hex conversion: $rgbHex"
 
 	def commandName = "HSBColor";
-	def payload = "${boundedHue},${boundedSaturation},${device[0].currentlevel}";
+	def payload = "${boundedHue},${boundedSaturation},${switchLevel.level.value}";
 
 	doLogging "COMMAND: $commandName ($payload)"
 
