bbc3bc4e107f908c653052e21fd4a9657d3429e3
DavinKD <37185595+DavinKD@users.noreply.github.com>
2021-08-30 20:36:26 -0700

    Update mqtt-motion.groovy

--- devicetypes/davindameron/mqtt-motion.src/mqtt-motion.groovy
+++ devicetypes/davindameron/mqtt-motion.src/mqtt-motion.groovy
@@ -4,9 +4,7 @@ metadata {
 	//Based on work by Brett Sheleski for Tasomota-Power
 
 	definition(name: "MQTT Motion", namespace: "davindameron", author: "Davin Dameron", ocfDeviceType: "x.com.st.d.sensor.contact", mnmn:"SmartThings", vid:"SmartThings-smartthings-SmartSense_Multi_Sensor") {
-		capability "Contact Sensor"
-		capability "Acceleration Sensor"
-		capability "Three Axis"
+		capability "Motion Sensor"
 		capability "Temperature Measurement"
 		capability "Sensor"
 		capability "Execute"
@@ -15,18 +13,14 @@ metadata {
 	}
 
 	tiles(scale: 2) {
-		multiAttributeTile(name:"contact", type: "generic", width: 6, height: 4) {
-			tileAttribute("device.contact", key: "PRIMARY_CONTROL") {
-				attributeState("open", label: 'Open', icon: "st.contact.contact.open", backgroundColor: "#e86d13")
-				attributeState("closed", label: 'Closed', icon: "st.contact.contact.closed", backgroundColor: "#00a0dc")
+		multiAttributeTile(name: "motion", type: "generic", width: 6, height: 4) {
+			tileAttribute("device.motion", key: "PRIMARY_CONTROL") {
+				attributeState "active", label: 'motion', icon: "st.motion.motion.active", backgroundColor: "#00A0DC"
+				attributeState "inactive", label: 'no motion', icon: "st.motion.motion.inactive", backgroundColor: "#cccccc"
 			}
 		}
-		standardTile("acceleration", "device.acceleration", width: 2, height: 2) {
-			state("active", label: 'Active', icon: "st.motion.acceleration.active", backgroundColor: "#00a0dc")
-			state("inactive", label: 'Inactive', icon: "st.motion.acceleration.inactive", backgroundColor: "#cccccc")
-		}
 		valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state("temperature", label: '${currentValue}°',
+			state("temperature", label: '${currentValue}°', unit: "F",
 					backgroundColors: [
 							[value: 31, color: "#153591"],
 							[value: 44, color: "#1e9cbb"],
@@ -41,10 +35,9 @@ metadata {
 		valueTile("battery", "device.battery", decoration: "flat", inactiveLabel: false, width: 2, height: 2) {
 			state "battery", label: '${currentValue}% battery', unit: ""
 		}
-		main(["contact", "acceleration", "temperature"])
-		details(["contact", "acceleration", "temperature", "battery"])
-	}
-    preferences {
+		main(["motion", "temperature"])
+		details(["motion", "temperature", "battery"])
+	}    preferences {
         
 	input(name: "MQTTProxy", type: "string", title: "MQTT Proxy Web Server", description: "MQTT Proxy Web Server", displayDuringSetup: true, required: false)
 	input(name: "MQTTTopic", type: "string", title: "MQTT Topic", description: "MQTT Topic", displayDuringSetup: true, required: false)
@@ -60,24 +53,13 @@ def execute(String command){
 			def json = new groovy.json.JsonSlurper().parseText(command);
 			if (json) {
 				doLogging("execute: Values received: ${json}")
-				if (json."contact"!=null) {
-					if (json."contact"==true) {
-						sendEvent(name: "contact", value: "closed")
+				if (json."occupancy"!=null) {
+					if (json."occupancy"==true) {
+						sendEvent(name: "motion", value: "active")
 					}
-					if (json."contact"==false) {
-						sendEvent(name: "contact", value: "open")
+					if (json."occupancy"==false) {
+						sendEvent(name: "motion", value: "inactive")
 					}
-				}
-				if (json."moving"!=null) {
-					if (json."moving"==true) {
-						sendEvent(name: "acceleration", value: "active")
-					}
-					if (json."contact"==false) {
-						sendEvent(name: "acceleration", value: "inactive")
-					}
-				}
-				if (json."x_axis"!=null) {
-					sendEvent(name: "threeAxis", value: json."x_axis"+json."y_axis"+json."z_axis")
 				}
 				if (json."battery"!=null) {
 					sendEvent(name: "battery", value: json."battery")
